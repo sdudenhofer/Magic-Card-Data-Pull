009923a84a429824a2d8d25f1ac1d782a974ef40
Seth Dudenhofer <sdudenhofer@gmail.com>
2024-11-29 18:33:24 -0800

    updated error handling

--- magic-api.py
+++ magic-api.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI, Response, Depends
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler
+)
 from fastapi.exceptions import RequestValidationError, HTTPException
 from fastapi.responses import PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from sqlmodel import Session, select, SQLModel
 from dotenv import load_dotenv
 import os
@@ -11,9 +16,17 @@ import json
 
 app = FastAPI()
 
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request, exc):
+    print(f'HTTP Error:  {repr(exc)})')
+    return await http_exception_handler(request, exc)
+
+
 @app.exception_handler(RequestValidationError)
-def validation_exception_handler(request, exc):
-    return PlainTextResponse(str(exc), status_code=400)
+async def custom_http_exception_handler(request, exc):
+    print(f'Validation Error:  {repr(exc)})')
+    return await request_validation_exception_handler(request, exc)
+
 
 class CardData(SQLModel):
     name: str
@@ -30,10 +43,11 @@ async def read_root():
 @app.get("/cards/", response_model=list[CardData])
 def get_all_cards(all_cards, session: Session = Depends(get_session)):
     data = "all_cards"
-    statement = select(data)
+    statement = session.select(data)
+    print(statement)
     get_cards = session.exec(statement).all()
     if not get_cards:
-        raise HTTPException(status_code=404, detail="No cards found")
+        raise HTTPException(status_code=410, detail="No cards found")
     results = [card for card in get_cards]
     return JSONResponse(content=jsonable_encoder(results))
 
